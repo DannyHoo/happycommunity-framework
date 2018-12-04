c24e753efe1e7367a4abd2d29abbbcf13d881105
yuyang.hu <huyuyang@lxfintech.com>
2018-12-04 11:41:38 +0800

    gateway、business、user 登录、注册

--- framework-common/src/main/java/com/happycommunity/framework/common/model/result/BaseResult.java
+++ framework-common/src/main/java/com/happycommunity/framework/common/model/result/BaseResult.java
@@ -1,38 +1,14 @@
 package com.happycommunity.framework.common.model.result;
 
-import com.happycommunity.framework.common.model.enums.ResultStatusEnumInterface;
-
 import java.io.Serializable;
 
 /**
  * @author Danny
  * @Title: BaseResult
  * @Description:
- * @Created on 2018-11-29 12:56:31
+ * @Created on 2018-12-03 17:56:30
  */
 public class BaseResult implements Serializable {
 
     private static final long serialVersionUID = 7567039181931362380L;
-
-    private ResultStatusEnumInterface resultStatusEnum;
-
-    public BaseResult() {
-    }
-
-    public BaseResult(ResultStatusEnumInterface resultStatusEnum) {
-        this.resultStatusEnum = resultStatusEnum;
-    }
-
-    public static long getSerialVersionUID() {
-        return serialVersionUID;
-    }
-
-    public ResultStatusEnumInterface getResultStatusEnum() {
-        return resultStatusEnum;
-    }
-
-    public BaseResult setResultStatusEnum(ResultStatusEnumInterface resultStatusEnum) {
-        this.resultStatusEnum = resultStatusEnum;
-        return this;
-    }
 }
--- framework-core/src/main/java/com/happycommunity/framework/core/util/RSAUtil.java
+++ framework-core/src/main/java/com/happycommunity/framework/core/util/RSAUtil.java
@@ -8,8 +8,10 @@ import java.security.InvalidKeyException;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 /**
  * @author Danny
@@ -19,17 +21,58 @@ import java.security.spec.PKCS8EncodedKeySpec;
  */
 public class RSAUtil {
 
+    public static String encrypt(String content, String publicKey) throws Exception {
+        String encryptedContent = encrypt(loadPublicKey(publicKey),content.getBytes());
+        return encryptedContent;
+    }
+
     /**
      * 解密经过RSA加密后的数据
      *
-     * @param password
+     * @param content
      * @param privateKey
      * @return
      * @throws Exception
      */
-    public static String decrypt(String password, String privateKey) throws Exception {
-        String decryptedPassword = decrypt(loadPrivateKey(privateKey), strToBase64(password));
-        return decryptedPassword;
+    public static String decrypt(String content, String privateKey) throws Exception {
+        String decryptedContent = decrypt(loadPrivateKey(privateKey), strToBase64(content));
+        return decryptedContent;
+    }
+
+    /**
+     * 公钥加密过程
+     *
+     * @param publicKey
+     *            公钥
+     * @param plainTextData
+     *            明文数据
+     * @return
+     * @throws Exception
+     *             加密过程中的异常信息
+     */
+    public static String encrypt(RSAPublicKey publicKey, byte[] plainTextData) throws Exception {
+        if (publicKey == null) {
+            throw new Exception("加密公钥为空, 请设置");
+        }
+        Cipher cipher = null;
+        try {
+            // 使用默认RSA
+            cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            byte[] output = cipher.doFinal(plainTextData);
+            return base64ToStr(output);
+        } catch (NoSuchAlgorithmException e) {
+            throw new Exception("无此加密算法");
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+            return null;
+        } catch (InvalidKeyException e) {
+            throw new Exception("加密公钥非法,请检查");
+        } catch (IllegalBlockSizeException e) {
+            throw new Exception("明文长度非法");
+        } catch (BadPaddingException e) {
+            throw new Exception("明文数据已损坏");
+        }
     }
 
     /**
@@ -91,6 +134,33 @@ public class RSAUtil {
         } catch (NullPointerException e) {
             throw new Exception("私钥数据为空", e);
         }
+    }
+
+    /**
+     * 从字符串中加载公钥
+     *
+     * @param publicKeyStr
+     *            公钥字符串
+     * @return
+     * @throws Exception
+     */
+    public static RSAPublicKey loadPublicKey(String publicKeyStr) throws Exception {
+        try {
+            byte[] buffer = javax.xml.bind.DatatypeConverter.parseBase64Binary(publicKeyStr);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(buffer);
+            return (RSAPublicKey) keyFactory.generatePublic(keySpec);
+        } catch (NoSuchAlgorithmException e) {
+            throw new Exception("无此算法", e);
+        } catch (InvalidKeySpecException e) {
+            throw new Exception("公钥非法", e);
+        } catch (NullPointerException e) {
+            throw new Exception("公钥数据为空", e);
+        }
+    }
+
+    public static String base64ToStr(byte[] b) {
+        return javax.xml.bind.DatatypeConverter.printBase64Binary(b);
     }
 
     public static byte[] strToBase64(String str) {
